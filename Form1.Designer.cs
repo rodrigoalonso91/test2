ea1622e5f6f1e58645d12ee208a57b5062bcb9a9
Rodrigo Alonso <rodrigoalonso.hr@gmail.com>
2021-09-13 20:14:31 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace test2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_SaveText = new System.Windows.Forms.Button();
+            this.Lbl_1 = new System.Windows.Forms.Label();
+            this.Txtbox_content = new System.Windows.Forms.TextBox();
+            this.SaveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // Btn_SaveText
+            // 
+            this.Btn_SaveText.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Btn_SaveText.Location = new System.Drawing.Point(262, 352);
+            this.Btn_SaveText.Name = "Btn_SaveText";
+            this.Btn_SaveText.Size = new System.Drawing.Size(270, 64);
+            this.Btn_SaveText.TabIndex = 0;
+            this.Btn_SaveText.Text = "Save text";
+            this.Btn_SaveText.UseVisualStyleBackColor = true;
+            this.Btn_SaveText.Click += new System.EventHandler(this.Btn_SaveText_Click);
+            // 
+            // Lbl_1
+            // 
+            this.Lbl_1.AutoSize = true;
+            this.Lbl_1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Lbl_1.Location = new System.Drawing.Point(12, 32);
+            this.Lbl_1.Name = "Lbl_1";
+            this.Lbl_1.Size = new System.Drawing.Size(109, 17);
+            this.Lbl_1.TabIndex = 1;
+            this.Lbl_1.Text = "Write something:";
+            // 
+            // Txtbox_content
+            // 
+            this.Txtbox_content.Location = new System.Drawing.Point(12, 76);
+            this.Txtbox_content.Multiline = true;
+            this.Txtbox_content.Name = "Txtbox_content";
+            this.Txtbox_content.Size = new System.Drawing.Size(776, 247);
+            this.Txtbox_content.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Txtbox_content);
+            this.Controls.Add(this.Lbl_1);
+            this.Controls.Add(this.Btn_SaveText);
+            this.Name = "MainForm";
+            this.Text = "Test3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_SaveText;
+        private System.Windows.Forms.Label Lbl_1;
+        private System.Windows.Forms.TextBox Txtbox_content;
+        private System.Windows.Forms.SaveFileDialog SaveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace test2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void Btn_SaveText_Click(object sender, EventArgs e)
+        {
+            SaveText(Txtbox_content.Text);
+        }
+        private void SaveText(string textContent)
+        {
+            var isTextEmpty = String.IsNullOrWhiteSpace(Txtbox_content.Text);
+
+            if (isTextEmpty)
+            {
+                MessageBox.Show("You must write something first.", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (SaveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var savePath = SaveFileDialog1.FileName + ".txt";
+                File.WriteAllText(savePath, textContent.Trim());
+                MessageBox.Show("The file was created", "Test 2", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
